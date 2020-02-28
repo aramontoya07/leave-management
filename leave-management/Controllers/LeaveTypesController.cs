d3c1f7ed8474c495fbd14e22be314bd420c1c573
aramontoya07 <aramontoya07@gmail.com>
2020-02-28 20:47:09 -0300

    setup de create view

--- leave-management/Controllers/LeaveTypesController.cs
+++ leave-management/Controllers/LeaveTypesController.cs
@@ -26,7 +26,7 @@ namespace leave_management.Controllers
         public ActionResult Index()
         {
             var leavetypes = _repo.FindAll().ToList();
-            var model = _mapper.Map<List<LeaveType>, List<DetailsLeaveTypeVM>>(leavetypes);
+            var model = _mapper.Map<List<LeaveType>, List<LeaveTypeVM>>(leavetypes);
             return View(model);
         }
 
@@ -45,11 +45,25 @@ namespace leave_management.Controllers
         // POST: LeaveTypes/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(LeaveTypeVM model)
         {
             try
             {
                 // TODO: Add insert logic here
+                if(!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var leaveType = _mapper.Map<LeaveType>(model);
+                leaveType.DateCreated = DateTime.Now;
+                var isSuccess =_repo.Create(leaveType); //recordar que devuelve booleano
+
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something went wrong...");
+                    return View(model);
+                }
 
                 return RedirectToAction(nameof(Index));
             }
--- leave-management/Mappings/Maps.cs
+++ leave-management/Mappings/Maps.cs
@@ -12,8 +12,7 @@ namespace leave_management.Mappings
     {
         public Maps()
         {
-            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap(); //crea un map entre la data class y la clase destino, reverse map permite que vaya en ambas direcciones
-            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap(); //crea un map entre la data class y la clase destino, reverse map permite que vaya en ambas direcciones
             CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
             CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
             CreateMap<Employee, EmployeeVM>().ReverseMap();
