dda719039b77bad5782eb17d8be3d2de45c523b0
aramontoya07 <aramontoya07@gmail.com>
2020-03-07 19:44:57 -0300

    control de acceso de usuarios con autorizacion de roles

--- leave-management/Controllers/LeaveTypesController.cs
+++ leave-management/Controllers/LeaveTypesController.cs
@@ -6,11 +6,13 @@ using AutoMapper;
 using leave_management.Contracts;
 using leave_management.Data;
 using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace leave_management.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class LeaveTypesController : Controller
 
     {
@@ -22,6 +24,7 @@ namespace leave_management.Controllers
             _repo = repo;
             _mapper = mapper;
         }
+        
         // GET: LeaveTypes
         public ActionResult Index()
         {
