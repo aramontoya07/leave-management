0d37ece92f7740c1cbef5a6e59dbe99684952a33
aramontoya07 <aramontoya07@gmail.com>
2020-03-06 23:29:26 -0300

    comienzo con los roles

---
+++ leave-management/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        public static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if(userManager.FindByNameAsync("admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admin@localhost"
+                };
+
+                var result = userManager.CreateAsync(user, "P@ssword1").Result;
+                if (result.Succeeded) //entonces asocio el usuario a un rol
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole //si el rol no existe lo creo y lo llamo administrador
+                {
+                    Name = "Administrator"
+                };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole //si el rol no existe lo creo y lo llamo Employee
+                {
+                    Name = "Employee"
+                };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
